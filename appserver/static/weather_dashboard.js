52c5297a9dbf4849f78a859c44be05c659101730
Simon Balz <simon@balz.me>
2014-06-16 17:09:20 +0200

    Added pressure_area field to the datamodel and added a fancy image based on this value to the weather dashboard

---
+++ appserver/static/weather_dashboard.js
@@ -0,0 +1,21 @@
+require(['jquery', 'underscore', 'splunkjs/mvc','splunkjs/mvc/utils','splunkjs/mvc/simplexml/ready!'], function($, _, mvc, utils){
+
+	var submittedTokens = mvc.Components.getInstance('submitted');
+	console.debug("submittedTokens", submittedTokens);
+
+	var searchManager = mvc.Components.getInstance(mvc.Components.getInstance('pressure').managerid)
+	var pressureData = searchManager.data('results', {
+    	output_mode: 'json_rows',
+    	count: 0
+	});
+
+	pressureData.on('data', function(results) { 
+		if (!pressureData.hasData()) {
+        	return;
+    	}
+    	var fields = _.map(results.collection().toJSON(), function(item) { return { pressure_area : item.pressure_area}; });
+    	console.debug("fields", fields);
+    	$("#pressure .panel-body").prepend('<div id="pressure_icon"><img src="/static/app/netatmo/' + fields[0].pressure_area +'.png" /></div>');
+	});
+
+});
